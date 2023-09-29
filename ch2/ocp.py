48a67ec9ac8877c793dd17e32fc8be5a5026db90
kamon <kamon.ayeva@gmail.com>
2023-09-29 22:39:56 +0200

    Update the example

--- ch2/ocp.py
+++ ch2/ocp.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Shape:
     def area(self):
         pass
@@ -12,10 +15,23 @@ class Rectangle(Shape):
         return self.width * self.height
 
 
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius**2)
+
+
 def calculate_area(shape):
     return shape.area()
 
 
 if __name__ == "__main__":
     rect = Rectangle(12, 8)
+    print("Rectangle area")
     print(calculate_area(rect))
+
+    print("Circle area")
+    circ = Circle(11)
+    print(calculate_area(circ))
