06ee4026b9f0ff2500cb8ed6964157fa0eab737a
kamon <kamon.ayeva@gmail.com>
2024-05-03 22:19:39 +0200

    Improve DI example code

--- ch10/dependency_injection/di_with_mock.py
+++ ch10/dependency_injection/di_with_mock.py
@@ -1,27 +1,19 @@
 from typing import Protocol
 
 
-class WeatherApi(Protocol):
+class WeatherApiClient(Protocol):
     def fetch_weather(self, location):
         """Fetch weather data for a given location"""
         ...
 
 
-class RealWeatherApi:
+class RealWeatherApiClient:
     def fetch_weather(self, location):
-        # In a real scenario, this method would perform
-        # an API call to a weather service
         return f"Real weather data for {location}"
 
 
-class MockWeatherApi:
-    def fetch_weather(self, location):
-        # Return a fixed response that can be used in tests
-        return f"Mock weather data for {location}"
-
-
 class WeatherService:
-    def __init__(self, weather_api: WeatherApi):
+    def __init__(self, weather_api: WeatherApiClient):
         self.weather_api = weather_api
 
     def get_weather(self, location):
@@ -29,5 +21,5 @@ class WeatherService:
 
 
 if __name__ == "__main__":
-    real_weather_service = WeatherService(RealWeatherApi())
-    print(real_weather_service.get_weather("Paris"))
+    ws = WeatherService(RealWeatherApiClient())
+    print(ws.get_weather("Paris"))
--- ch10/dependency_injection/test_di_with_mock.py
+++ ch10/dependency_injection/test_di_with_mock.py
@@ -1,10 +1,16 @@
 import unittest
-from di_with_mock import MockWeatherApi, WeatherService
+from di_with_mock import WeatherService
+
+
+class MockWeatherApiClient:
+    def fetch_weather(self, location):
+        # Return a fixed response that can be used in tests
+        return f"Mock weather data for {location}"
 
 
 class TestWeatherService(unittest.TestCase):
     def test_get_weather(self):
-        mock_api = MockWeatherApi()
+        mock_api = MockWeatherApiClient()
         weather_service = WeatherService(mock_api)
         self.assertEqual(
             weather_service.get_weather("Anywhere"),
