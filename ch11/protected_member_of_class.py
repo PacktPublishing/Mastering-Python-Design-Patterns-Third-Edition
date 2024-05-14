fe8eea228ff7c2ff6fe7f4ccff0b6081a694deb0
kamon <kamon.ayeva@gmail.com>
2024-05-14 14:18:04 +0200

    Changed the example

--- ch11/protected_member_of_class.py
+++ ch11/protected_member_of_class.py
@@ -1,23 +1,48 @@
-class Rectangle:
-    def __init__(self, width, height):
-        self._width = width
-        self._height = height
+class Book:
+    def __init__(self, title, author, isbn):
+        self._title = title
+        self._author = author
+        self._isbn = isbn
 
 
-class BetterRectangle:
-    def __init__(self, width, height):
-        self._width = width
-        self._height = height
+class BetterBook:
+    def __init__(self, title, author, isbn):
+        self._title = title
+        self._author = author
+        self._isbn = isbn
 
-    def area(self):
-        return self._width * self._height
+    def presentation_line(self):
+        return f"{self._title} by {self._author}"
+
+    def details(self):
+        return {
+            "name": self._title,
+            "author": self._author,
+            "isbn": self._isbn,
+        }
 
 
 if __name__ == "__main__":
-    r = Rectangle(5, 6)
-    print("Direct access of protected members")
-    print(f"Rectangle area: {r._width * r._height} m2")
+    b1 = Book(
+        "Mastering Object-Oriented Python",
+        "Steven F. Lott",
+        "9781789531367",
+    )
+    print(
+        "Bad practice: Direct access of protected members"
+    )
+    print(b1._title, b1._author, b1._isbn)
 
-    r = BetterRectangle(5, 6)
-    print("Recommended: Access via the public interface")
-    print(f"Rectangle area: {r.area()} m2")
+    b2 = BetterBook(
+        "Python Algorithms",
+        "Magnus Lie Hetland",
+        "9781484200568",
+    )
+    print(
+        "Recommended: Access via the public interface"
+    )
+    print(b2.presentation_line())
+    print("All details:")
+    details = b2.details()
+    for key in details:
+        print(f"  {key}: {details[key]}")
