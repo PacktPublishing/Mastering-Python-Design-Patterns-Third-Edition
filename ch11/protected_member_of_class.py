ca85e1db18ecca6ab999f95383f0681c00fe86ca
kamon <kamon.ayeva@gmail.com>
2024-05-15 10:02:45 +0200

    Improve example for consistency

--- ch11/protected_member_of_class.py
+++ ch11/protected_member_of_class.py
@@ -1,48 +1,29 @@
 class Book:
-    def __init__(self, title, author, isbn):
+    def __init__(self, title, author):
         self._title = title
         self._author = author
-        self._isbn = isbn
 
 
 class BetterBook:
-    def __init__(self, title, author, isbn):
+    def __init__(self, title, author):
         self._title = title
         self._author = author
-        self._isbn = isbn
 
     def presentation_line(self):
         return f"{self._title} by {self._author}"
-
-    def details(self):
-        return {
-            "name": self._title,
-            "author": self._author,
-            "isbn": self._isbn,
-        }
 
 
 if __name__ == "__main__":
     b1 = Book(
         "Mastering Object-Oriented Python",
         "Steven F. Lott",
-        "9781789531367",
     )
-    print(
-        "Bad practice: Direct access of protected members"
-    )
-    print(b1._title, b1._author, b1._isbn)
+    print("Bad practice: Direct access of protected members")
+    print(f"{b1._title} by {b1._author}")
 
     b2 = BetterBook(
         "Python Algorithms",
         "Magnus Lie Hetland",
-        "9781484200568",
     )
-    print(
-        "Recommended: Access via the public interface"
-    )
+    print("Recommended: Access via the public interface")
     print(b2.presentation_line())
-    print("All details:")
-    details = b2.details()
-    for key in details:
-        print(f"  {key}: {details[key]}")
